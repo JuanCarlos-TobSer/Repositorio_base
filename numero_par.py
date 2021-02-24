72b9ca7c56f81d5ebaa0d9289bf504588249fa8a
JuanCarlos-TobSer <tobonjuan7248@americana.edu.co>
2021-02-23 19:23:33 -0500

    Agregando archivo de python

---
+++ numero_par.py
@@ -0,0 +1,14 @@
+'''Pedir dos numeros y determinar cual es par,
+o si ambos lo son.'''
+
+num1 = float(input("Digite primer numero: "))
+num2 = float(input("Digite segundo numero: "))
+
+if num1%2==0 and num2%2==0:
+	print("Ambos numeros son pares")
+elif num1%2==0:
+	print("El primer numero es par")
+elif num2%2==0:
+	print("El segundo numero es par")
+else:
+	print("Ambos numeros son impares")
